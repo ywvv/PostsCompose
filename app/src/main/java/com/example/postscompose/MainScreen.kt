b3721bb66adcae41a9369307705f32c18d8069e0
Vladyslav Horban <ywvvvw@gmail.com>
2023-08-02 20:34:47 +0300

    add FloatingActionButton and Snackbar

--- app/src/main/java/com/example/postscompose/MainScreen.kt
+++ app/src/main/java/com/example/postscompose/MainScreen.kt
@@ -2,24 +2,39 @@ package com.example.postscompose
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.postscompose.ui.theme.NavigationItem
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
         bottomBar = {
             NavigationBar {
@@ -50,7 +65,29 @@ fun MainScreen() {
                         })
                 }
             }
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        val action = snackbarHostState.showSnackbar(
+                            message = "This is snackbar",
+                            actionLabel = "Hide FAB",
+                            duration = SnackbarDuration.Long
+                        )
+                        if (action == SnackbarResult.ActionPerformed) {
+                            fabIsVisible.value = false
+                        }
+                    }
+                }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
         }
+
     ) {
         Column(
             modifier = Modifier.padding(it)
